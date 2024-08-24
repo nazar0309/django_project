35618d50c969157a8456544a4cbeae1674657e43
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-08-24 20:15:51 +0100

    Add view to hello_word

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse('Hello, World!')
+def index(request):
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
