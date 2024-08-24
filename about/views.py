6eaa11ec55c96cc2719e67c92e0ea5f3dc9ea000
Nazar Tsuper <nazartsuper0309@gmail.com>
2024-08-24 19:58:35 +0100

    Add urls

--- about/views.py
+++ about/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+def about_me(response):
+    return HttpResponse("This would be the about page")
+
